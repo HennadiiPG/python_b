9ae0e055331ab2cd7d8f35e991d15076a99a33f5
Optimist <podzarovgennadij@gmail.com>
2022-07-26 22:30:02 +0300

    save

---
+++ lesson_3/lesson_3_part_1.py
@@ -0,0 +1,13 @@
+
+integer = input("Привет, меня зовут Гена!\nА ты кто?")
+var = ("Добро пожаловать" + integer)
+
+print(var)
+
+integer = input("Загадай число:")
+var_1 = int(integer)
+
+integer = input("Загадай ещё число:")
+var_2 = int(integer)
+
+print("Их сумма равна", var_1 + var_2)
