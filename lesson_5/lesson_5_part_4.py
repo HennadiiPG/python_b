8462a349d63c4dcc650d809027356a7906571fbe
Optimist <podzarovgennadij@gmail.com>
2022-08-04 18:11:57 +0300

    les_5_part_4

--- lesson_5/lesson_5_part_4.py
+++ lesson_5/lesson_5_part_4.py
@@ -1,8 +1,5 @@
 n = int(input("Введите число:"))
-for k in range(1,n+1):
- # p = k
-  #print(p, "p")
-  i = k ** 2
-  #print(i, "i")
-  if i <= n:
-    print(i)
+for k in range(1,n + 1):
+    i = k ** 2
+    if i <= n:
+        print(i)
