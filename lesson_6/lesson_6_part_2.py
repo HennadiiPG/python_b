cec7907cdc50028c3f5313430d599082982cd2ef
Optimist <podzarovgennadij@gmail.com>
2022-08-06 19:06:24 +0300

    ДЗ 6.1 while True

---
+++ lesson_6/lesson_6_part_2.py
@@ -0,0 +1,13 @@
+while True:
+    string = input("Введите строку: ") #строка
+    char = input("Введите символ: ") #символ
+    if string.find(char) == -1:
+        print("Данного символа нет в строке:")
+        break
+    elif
+    n = string.find(char)
+    len(string) #Длина строки
+    print(len(string))
+
+    string.find(char, n)
+    print(string.find(char, n))
