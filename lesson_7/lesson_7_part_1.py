8d6105e38b99fdda9b16f68a1339189d0217bc46
Optimist <podzarovgennadij@gmail.com>
2022-08-09 22:47:22 +0300

    ДЗ 7.1

---
+++ lesson_7/lesson_7_part_1.py
@@ -0,0 +1,7 @@
+lst = list(input("Введите список из чисел: "))
+index = int(input("Введите индекс элемента из списка выше: "))
+for n in range(index, len(lst) - 1):
+    lst[n] = lst[n + 1]
+    #print(lst)
+lst.pop()
+print(' '.join([str(n) for n in lst]))
