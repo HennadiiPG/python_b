314ef5c6c182388fa3f765350384618c7642b442
Optimist <podzarovgennadij@gmail.com>
2022-08-09 23:30:51 +0300

    ДЗ 7.2

---
+++ lesson_7/lesson_7_part_2.py
@@ -0,0 +1,9 @@
+lst = list(input("Введите список из чисел: "))
+k = int(input("Введите индекс элемента из списка выше (k): "))
+с = int(input("Введите число которое хотите добавить в список (C): "))
+lst.append(0)
+for i in range(len(lst) - 1, k, -1):
+    lst[i] = lst[i-1]
+lst[k] = с
+
+print(' '.join([str(i) for i in lst]))
