57d92d037ab75530924fb409324b0f711fb61665
HennadiiPG <109980053+HennadiiPG@users.noreply.github.com>
2022-08-10 13:21:17 +0300

    Update lesson_7_part_1.py
    Без генератора списков

--- lesson_7/lesson_7_part_1.py
+++ lesson_7/lesson_7_part_1.py
@@ -1,6 +1,6 @@
-lst = [int(s) for s in input("Введите список из чисел: ").split()]
-k = int(input("Введите индекс элемента из списка выше: "))
-for i in range(k + 1, len(lst)):
+lst = input("Введите список из чисел: ").split()
+index = int(input("Введите индекс элемента из списка выше: "))
+for i in range(index + 1, len(lst)):
     lst[i - 1] = lst[i]
 lst.pop()
-print(' '.join([str(i) for i in lst]))
+print(lst)
