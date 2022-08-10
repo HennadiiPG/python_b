c39b2679dd75d45429e0dad40014425bf4a9880d
HennadiiPG <109980053+HennadiiPG@users.noreply.github.com>
2022-08-10 13:25:12 +0300

    Update lesson_7_part_2.py

--- lesson_7/lesson_7_part_2.py
+++ lesson_7/lesson_7_part_2.py
@@ -1,4 +1,4 @@
-lst = [int(s) for s in input("Введите список из чисел: ").split()]
+lst = input("Введите список из чисел: ").split()
 k = int(input("Введите индекс элемента из списка выше (k): "))
 с = int(input("Введите число которое хотите добавить в список (C): "))
 lst.append(0)
@@ -6,4 +6,4 @@ for i in range(len(lst) - 1, k, -1):
     lst[i] = lst[i-1]
 lst[k] = с
 
-print(' '.join([str(i) for i in lst]))
+print(lst)
