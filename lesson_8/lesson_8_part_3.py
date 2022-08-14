6087cf9e9f91b8bf6c0bbac8a90f4b9eb8e229fd
Optimist <podzarovgennadij@gmail.com>
2022-08-14 14:05:17 +0300

    ДЗ 8.3 генератор

--- lesson_8/lesson_8_part_3.py
+++ lesson_8/lesson_8_part_3.py
@@ -1,16 +1,6 @@
 import random
-Matrix = [random.randint(1, 10) for i in range(15)]
+Matrix = [random.randint(1, 10) for i in range(5)]
 print(Matrix)
-s = 0 #"четные"
-w = 0 # нечетные
-for i in Matrix:
-    if i % 2 == 0:
-        s += i
-    elif i % 2 != 0:
-        w += i
-print(s)
-print(w)
-if w > s:
-    print(True)
-else:
-    print(False)
+a = sum([i for i in Matrix if i % 2 == 0])
+b = sum([i for i in Matrix if i % 2 != 0])
+c = [print("Да") if a < b else print("Нет")]
