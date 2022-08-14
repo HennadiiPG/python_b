aae054a9c3c4034761e805163e1de6c7dc3d35f2
Optimist <podzarovgennadij@gmail.com>
2022-08-14 15:03:44 +0300

    ДЗ 8.2 генератор

--- lesson_8/lesson_8_part_2.py
+++ lesson_8/lesson_8_part_2.py
@@ -1,14 +1,9 @@
 import random
-s = 0
 n = int(input("Введите число для вывода матрицы: "))
 Matrix = [[random.randint(1, 10) for i in range(n)] for j in range(n)]
 for i in range(n):
     print(Matrix[i])
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            s += Matrix[i][j]
-print("Сумма чисел по диагонали матрицы = ",  s)
-
+a = [Matrix[i][j] for j in range(n) for i in range(n) if i == j]
+print("Сумма чисел диагонали матрицы = ", sum(a))
 w = sum(row[-1] for row in Matrix)
 print("Сумма чисел последнего столбца матрицы = ", w)
