b2bc7e3c0daab2cbb038b5a7ea69936fe20d5972
Optimist <podzarovgennadij@gmail.com>
2022-08-14 09:14:50 +0300

    ДЗ 8.1 (доп)

--- lesson_8/lesson_8_part_1.py
+++ lesson_8/lesson_8_part_1.py
@@ -1,5 +1,7 @@
+import random
+
 n = int(input("Введите число для вывода матрицы: "))
-a1 = [[i for i in range(j, j + n)] for j in range(0, n**2-1, n)]
+a1 = [[random.randrange(10) for i in range(n)] for j in range(n)]
 a2 = [i for i in range(-n, 0)]
 for i in range(len(a1)):
     if i % 2 == 0:
