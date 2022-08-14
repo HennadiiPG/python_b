ed77080ddb045432a52f79652d00b28d4dd6ba18
Optimist <podzarovgennadij@gmail.com>
2022-08-14 12:06:23 +0300

    ДЗ 8.1 Генератор списка

--- lesson_8/lesson_8_part_1.py
+++ lesson_8/lesson_8_part_1.py
@@ -1,12 +1,4 @@
-import random
-
 n = int(input("Введите число для вывода матрицы: "))
-a1 = [[random.randrange(10) for i in range(n)] for j in range(n)]
-a2 = [i for i in range(-n, 0)]
-for i in range(len(a1)):
-    if i % 2 == 0:
-        a1[i] = [i for _ in range(n)]
-    elif i % 2 != 0:
-        a1[i] = a2
-for b in a1:
-    print(b)
+b = [[i * -1 if j % 2 != 0 else j for i in range(n, 0, -1)] for j in range(n)]
+for a in b:
+    print(a)
