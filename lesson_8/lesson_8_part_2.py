60303b7659c8dddb0e60f996f8469a8c50a8debe
Optimist <podzarovgennadij@gmail.com>
2022-08-14 10:18:09 +0300

    ДЗ 8.2 (доп)

--- lesson_8/lesson_8_part_2.py
+++ lesson_8/lesson_8_part_2.py
@@ -1,6 +1,14 @@
 import random
-
-n = m = int(input("Введите число для вывода матрицы: "))
-Matrix = [[random.randint(1, 100) for j in range(n)] for i in range(m)]
-for i in range(m):
+s = 0
+n = int(input("Введите число для вывода матрицы: "))
+Matrix = [[random.randint(1, 10) for i in range(n)] for j in range(n)]
+for i in range(n):
     print(Matrix[i])
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            s += Matrix[i][j]
+print("Сумма чисел по диагонали матрицы = ",  s)
+
+w = sum(row[-1] for row in Matrix)
+print("Сумма чисел последнего столбца матрицы = ", w)
