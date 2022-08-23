3f12e6150021ba18079ac55522e9b3d391371804
Optimist <podzarovgennadij@gmail.com>
2022-08-23 19:02:59 +0300

    ДЗ 11.1

---
+++ lesson_11/lesson_11_part_1.py
@@ -0,0 +1,13 @@
+def func(x, y):
+    summ = False
+    for i in range(1, x + 1):
+        for n in range(1, x + 1):
+            if i + n == y:
+                summ = True
+                print(i, n)
+            break
+    return summ
+
+
+print(func(10, 21))
+print(func(12, 10))
