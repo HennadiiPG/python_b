a380b88714247763e67a1dd883c4fdacaab1e8fc
Optimist <podzarovgennadij@gmail.com>
2022-08-30 19:54:44 +0300

    ДЗ 11.4

--- lesson_11/lesson_11_part_4.py
+++ lesson_11/lesson_11_part_4.py
@@ -3,25 +3,22 @@ def spreadsheet(foo):
 
     def wrapper(*args, **kwargs):
         k = foo(*args, **kwargs)
-        k = list(k)
-        k.sort()
-        print(" ", "_" * len(str(k[-1])), " ", "_" * 12, " ", "_" * 9)
-        for i in range(len(k)):
-            print("|", str(k[i]).ljust(len(str(k[-1])), " "), "|", "Кратное 3".ljust(12, " ") if k[i] % 3 == 0
+        p = list(k)
+        p.sort()
+        print(" ", "_" * len(str(p[-1])), " ", "_" * 12, " ", "_" * 9)
+        for i in range(len(p)):
+            print("|", str(p[i]).ljust(len(str(p[-1])), " "), "|", "Кратное 3".ljust(12, " ") if p[i] % 3 == 0
                   else "Не кратное 3".rjust(12, " "),
-                  "|", "Парное".ljust(9, " ") if k[i] % 2 == 0 else "Не парное".ljust(9, " "), "|")
-            print("|", "_" * len(str(k[-1])), "|", "_" * 12, "|", "_" * 9, "|")
+                  "|", "Парное".ljust(9, " ") if p[i] % 2 == 0 else "Не парное".ljust(9, " "), "|")
+            print("|", "_" * len(str(p[-1])), "|", "_" * 12, "|", "_" * 9, "|")
     return wrapper
 
 
 @spreadsheet
 def table_values(x):
     import random
-    x = int(input("dfsdfsdffdfsdfd"))
-    n = set(random.randint(1, 10) for i in range(x))
+    n = set(random.randint(1, 100) for _ in range(1, x+1))
     return n
 
 
-table_values(5)
-
-
+table_values(7)
