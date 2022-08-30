4332956c78b0f084828a5ab37f68b758df3f4acf
Optimist <podzarovgennadij@gmail.com>
2022-08-30 19:57:54 +0300

    ДЗ 11.1

--- lesson_11/lesson_11_part_1.py
+++ lesson_11/lesson_11_part_1.py
@@ -4,10 +4,11 @@ def func(x, y):
         for n in range(1, x + 1):
             if i + n == y:
                 summa = True
-                print(i, n)
+                # print(i, n)
             break
     return summa
 
 
 print(func(10, 21))
+print()
 print(func(12, 10))
