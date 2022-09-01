6a780a239756191f5041d42acbac6ea5a338c146
HennadiiPG <109980053+HennadiiPG@users.noreply.github.com>
2022-09-01 07:46:59 +0300

    Update lesson_11_part_4.py

--- lesson_11/lesson_11_part_4.py
+++ lesson_11/lesson_11_part_4.py
@@ -1,15 +1,14 @@
-
-def spreadsheet(foo):
-
     def wrapper(*args, **kwargs):
         k = foo(*args, **kwargs)
         p = list(k)
         p.sort()
         print(" ", "_" * len(str(p[-1])), " ", "_" * 12, " ", "_" * 9)
         for i in range(len(p)):
-            print("|", str(p[i]).ljust(len(str(p[-1])), " "), "|", "Кратное 3".ljust(12, " ") if p[i] % 3 == 0
+            print("|", str(p[i]).ljust(len(str(p[-1])), " "), "|", 
+                  "Кратное 3".ljust(12, " ") if p[i] % 3 == 0
                   else "Не кратное 3".rjust(12, " "),
-                  "|", "Парное".ljust(9, " ") if p[i] % 2 == 0 else "Не парное".ljust(9, " "), "|")
+                  "|", "Парное".ljust(9, " ") if p[i] % 2 == 0 
+                  else "Не парное".ljust(9, " "), "|")
             print("|", "_" * len(str(p[-1])), "|", "_" * 12, "|", "_" * 9, "|")
     return wrapper
 
