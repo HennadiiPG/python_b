4b9d76be206b37d851ce4485a175c25a588c83bf
Optimist <podzarovgennadij@gmail.com>
2022-09-07 20:03:27 +0300

    Исправлено по PEP8

--- lesson_11/lesson_11_part_4.py
+++ lesson_11/lesson_11_part_4.py
@@ -1,23 +1,25 @@
+def spreadsheet(foo):
+
     def wrapper(*args, **kwargs):
         k = foo(*args, **kwargs)
         p = list(k)
         p.sort()
         print(" ", "_" * len(str(p[-1])), " ", "_" * 12, " ", "_" * 9)
         for i in range(len(p)):
-            print("|", str(p[i]).ljust(len(str(p[-1])), " "), "|", 
-                  "Кратное 3".ljust(12, " ") if p[i] % 3 == 0
+            print("|", str(p[i]).ljust(len(str(p[-1])), " "),
+                  "|", "Кратное 3".ljust(12, " ") if p[i] % 3 == 0
                   else "Не кратное 3".rjust(12, " "),
-                  "|", "Парное".ljust(9, " ") if p[i] % 2 == 0 
+                  "|", "Парное".ljust(9, " ") if p[i] % 2 == 0
                   else "Не парное".ljust(9, " "), "|")
             print("|", "_" * len(str(p[-1])), "|", "_" * 12, "|", "_" * 9, "|")
     return wrapper
 
 
 @spreadsheet
-def table_values(x):
+def numbers(x):
     import random
     n = set(random.randint(1, 100) for _ in range(1, x+1))
     return n
 
 
-table_values(7)
+numbers(4)
