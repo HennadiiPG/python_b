b4116c342d118bd88dbcbc3ebdbae383ed6a96bc
Optimist <podzarovgennadij@gmail.com>
2022-09-08 19:30:15 +0300

    package_demo

---
+++ package_demo/run_funk.py
@@ -0,0 +1,8 @@
+import package_demo.module as a
+import package_demo.module as b
+import package_demo.module as c
+for n in a.prost_chusla(1, 100):
+    print(n, end=" ")
+print()
+print(b.ekviv_sum_pervogo_chusla(10, 21))
+c.sort_parn_krat_3(3, 9)
